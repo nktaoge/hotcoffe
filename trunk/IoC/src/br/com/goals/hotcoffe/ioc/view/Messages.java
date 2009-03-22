6c1aad01695a367f55071d225a3af16a9f48554e
fabio.oshiro <fabio.oshiro@4d8a3da4-5952-0410-a00d-6df5d22118a9>
2009-03-22 15:44:31 +0000

    Internacionalizacao

---
+++ trunk/IoC/src/br/com/goals/hotcoffe/ioc/view/Messages.java
@@ -0,0 +1,18 @@
+package br.com.goals.hotcoffe.ioc.view;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) throws MissingResourceException {
+		return RESOURCE_BUNDLE.getString(key);
+	}
+}
