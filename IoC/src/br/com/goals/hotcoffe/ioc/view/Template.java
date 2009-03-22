6e2a4a469f2d21a2d0f1bd7e4023acd2bb24d5e8
fabio.oshiro <fabio.oshiro@4d8a3da4-5952-0410-a00d-6df5d22118a9>
2009-03-22 15:44:31 +0000

    Internacionalizacao

--- IoC/src/br/com/goals/hotcoffe/ioc/view/Template.java
+++ IoC/src/br/com/goals/hotcoffe/ioc/view/Template.java
@@ -1,6 +1,7 @@
 package br.com.goals.hotcoffe.ioc.view;
 
 import java.lang.reflect.Method;
+import java.util.MissingResourceException;
 
 import br.com.goals.hotcoffe.ioc.Controlador;
 import br.com.goals.hotcoffe.ioc.casosdeuso.UmCasoDeUso;
@@ -14,15 +15,22 @@ public class Template {
 	 * @return html do formulario
 	 */
 	public static String criarFormulario(Object obj,UmCasoDeUso umCasoDeUso) {
-		String form="<form action=\"?"+ Controlador.IOC_KEY +"="+umCasoDeUso.getKey()+"\" method=\"post\">";
+		String form="<form action=\"?"+ Controlador.IOC_KEY +"="+umCasoDeUso.getKey()+"\" method=\"post\">"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		Method[] metodos = obj.getClass().getMethods();
 		for (int i = 0; i < metodos.length; i++) {
 			String nome = metodos[i].getName();
-			if(nome.startsWith("set")){
-				form+="<div><label for=\"idField"+i+"\">" + metodos[i].getName() + "</label><input id=\"idField"+i+"\" name=\""+metodos[i].getName()+"\" /></div>";
+			if(nome.startsWith("set")){ //$NON-NLS-1$
+				form+="<div><label for=\"idField"+i+"\">" + getLabel(obj,nome) + "</label><input id=\"idField"+i+"\" name=\""+metodos[i].getName()+"\" /></div>"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 			}
 		}
-		form+="<input type=\"submit\"/></form>";		
+		form+="<input type=\"submit\"/></form>";		 //$NON-NLS-1$
 		return form;
+	}
+	private static String getLabel(Object obj,String nome){
+		try{
+			return Messages.getString("Template."+obj.getClass().getCanonicalName()+"."+nome); //$NON-NLS-1$
+		}catch(MissingResourceException e){
+			return nome.substring(3);
+		}
 	}
 }
