ca3803da9017a5aa4f6f849a1dfa4f5c2c1c02f6
fabio.oshiro <fabio.oshiro@4d8a3da4-5952-0410-a00d-6df5d22118a9>
2009-04-22 03:09:25 +0000

    Compilando no Ubuntu

--- IoC/src/br/com/goals/lnc/bo/Programador.java
+++ IoC/src/br/com/goals/lnc/bo/Programador.java
@@ -22,4 +22,32 @@ public class Programador {
 		FileUtils.writeStringToFile(arqJava, codigo);
 		Compilador.compilar(arqJava);
 	}
+	
+	/**
+	 * Cria um metodo que retorna um boolean na classe sigName,br>
+	 * recebe um Object como parametro
+	 * 
+	 * @param sigName nome da classe de significado
+	 * @param metodoVerbo nome do metodo
+	 * @throws Exception
+	 */
+	public void criarMetodo(String sigName, String metodoVerbo,String comments) throws Exception {
+		String metodoBody = "" +
+				"public boolean " + metodoVerbo + "(Object obj){\n" +
+				"\t\treturn true;\n" +
+				"\t}\n\n\t";
+		
+		
+		File arqJava = new File(Compilador.workspaceSrc + AnalisadorSemantico.SIG_SRC + sigName + ".java");
+		String codigo = FileUtils.readFileToString(arqJava);
+		logger.info(codigo);
+		codigo = codigo.replace("public "+sigName + "(){",
+				metodoBody + 
+				"public "+sigName + "(){"
+		);
+		logger.info(codigo);
+		
+		FileUtils.writeStringToFile(arqJava, codigo);
+		Compilador.compilar(arqJava);
+	}
 }
