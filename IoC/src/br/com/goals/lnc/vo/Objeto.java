aa597629d675302f0596db4b8aae3b3efd3d8230
fabio.oshiro <fabio.oshiro@4d8a3da4-5952-0410-a00d-6df5d22118a9>
2009-05-24 03:21:22 +0000

    Criado um objeto qualquer

---
+++ IoC/src/br/com/goals/lnc/vo/Objeto.java
@@ -0,0 +1,33 @@
+package br.com.goals.lnc.vo;
+
+import java.util.ArrayList;
+
+/**
+ * Tem metodos e atributos
+ * deve ter tambem algum valor padrao
+ * @author fabio
+ *
+ */
+public class Objeto {
+	/**
+	 * É um Atributo de
+	 */
+	private Objeto atributoDe = null;
+	
+	/**
+	 * Valor padrao do objeto
+	 */
+	private String valorPadrao = null;
+	
+	/**
+	 * Atributos de um objeto
+	 */
+	private ArrayList<Objeto> atributos = new ArrayList<Objeto>();
+	
+	/**
+	 * Metodos de um objeto qualquer
+	 */
+	private ArrayList<Metodo> metodos = new ArrayList<Metodo>();
+	
+	
+}
