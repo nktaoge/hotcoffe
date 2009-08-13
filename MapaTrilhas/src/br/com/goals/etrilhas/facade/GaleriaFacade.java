6dc28e9e598091c8dcdcf3135e9ce06bab232332
fabio.oshiro <fabio.oshiro@4d8a3da4-5952-0410-a00d-6df5d22118a9>
2009-08-13 23:34:16 +0000

    Btn de next e prev

--- MapaTrilhas/src/br/com/goals/etrilhas/facade/GaleriaFacade.java
+++ MapaTrilhas/src/br/com/goals/etrilhas/facade/GaleriaFacade.java
@@ -66,15 +66,21 @@ public class GaleriaFacade extends BaseFacade<Galeria>{
 		logger.debug("Criando galeria "  + itemId);
 		Galeria galeria = selecionar(gal.getId());
 		Foto primeiraFoto = galeria.getFotos().get(0);
-		String links = "";
-		int i=1;
-		for(Foto foto:galeria.getFotos()){
-			links+=" <a href=\"javascript:openFoto('"+foto.getUrlRelativaJpg()+"')\">"+i+"</a> ";
-			i++;
+		String links = "<a href=\"javascript:prevFoto()\" id=\"fotoAnterior\">&lt;&lt;</a> ";
+		String scriptArr = "<script>var arrFotosI=0; var arrFotos = new Array(";
+		for(int i=0;i<galeria.getFotos().size();i++){
+			Foto foto = galeria.getFotos().get(i);
+			scriptArr+="\"" + foto.getUrlRelativaJpg() + "\",";
 		}
+		scriptArr =scriptArr.substring(0,scriptArr.length()-1); 
+		scriptArr+=");</script>";
+		for(int i=0;i<galeria.getFotos().size();i++){
+			links+=" <a href=\"javascript:openFoto("+i+")\" id=\"pic"+i+"\">"+(i+1)+"</a> ";
+		}
+		links+="<a href=\"javascript:nextFoto()\" id=\"fotoPosterior\">&gt;&gt;</a>";
 		Template template = new Template();
 		template.setTemplateFile("galeria.html");
-		template.setArea("links", links);
+		template.setArea("links", scriptArr + links);
 		template.setArea("foto", "<img src=\""+primeiraFoto.getUrlRelativaJpg()+"\" height=\"300\" />");
 		template.setArea("descricao",primeiraFoto.getTxtDescricao());
 		File f = new File(galeriaDao.getBasePath()+"Galeria-"+itemId+".html");
